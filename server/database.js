7bc4f73758cd7055afa99e2aeadb35e1205e8855
roselineren <101987077+roselineren@users.noreply.github.com>
2023-03-13 16:58:52 +0100

    requete

--- server/database.js
+++ server/database.js
@@ -17,8 +17,34 @@ async function run() {
     const db = client.db(MONGODB_DB_NAME);
     const collection = db.collection('products');
 
-    const result = await collection.insertMany(products);
+    const result = await collection.insertMany(products, { upsert: false });
     console.log(result);
+
+    //Requete 1
+    const brand='dedicated'
+    const filter_brand = await collection.find({brand}).toArray();
+    //console.log(filter_brand);
+
+    //Requete 2
+    const p=50;
+    const filter_price=await collection.find({"price":{ $lt: p}}).toArray();
+    //console.log(filter_price);
+
+    //Requete 3
+    const order_price=await collection.aggregate([{$sort:{price : -1 }}]).toArray();
+    //console.log(order_price);
+
+    //Requete 4
+    const order_date=await collection.aggregate([{$sort:{date : -1 }}]).toArray();
+    //console.log(order_date);
+
+    // Requete 5
+    const now = new Date();
+    const twoWeeksAgo = new Date(now.getTime() - (14 * 24 * 60 * 60 * 1000));
+    const filter_2weeks=await collection.find({"date":{$gt: twoWeeksAgo.toISOString().slice(0, 10)}}).toArray();
+    console.log(filter_2weeks);
+
+
 
   } catch (e) {
     console.error(e);
