1aaed9c851e816db5001d0a3e853e7ab22a326b6
roselineren <101987077+roselineren@users.noreply.github.com>
2023-03-21 19:55:35 +0100

    finit de get the product api

--- server/api.js
+++ server/api.js
@@ -1,68 +1,71 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const {MongoClient, ObjectId} = require('mongodb');
 
-const BodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
-
-const CONNECTION_URL = "mongodb+srv://roselineren:Ren750114802@cluster0.rcbfex1.mongodb.net/?retryWrites=true&w=majority";
-const DATABASE_NAME = "clearfashion";
-
+const MONGODB_URI = "mongodb+srv://roselineren:Ren750114802@cluster0.rcbfex1.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_DB_NAME = 'clearfashion'
 const PORT = 8092;
+
 const app = express();
-
 module.exports = app;
-
-app.use(require('body-parser').json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 
-app.options('*', cors());
+let database, collection;
 
-app.use(BodyParser.urlencoded({ extended: true }));
-
-var database, collection;
-
-app.get('/products', (request, response) => {
-  response.send({'ack': true});
+MongoClient.connect(MONGODB_URI, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    throw error;
+  }
+  database = client.db(MONGODB_DB_NAME);
+  collection = database.collection('products');
+  console.log(`Connected to '${MONGODB_DB_NAME}'!`);
 });
 
-/*app.get("/products/:id", (request, response) => {
-  collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
-      if(error) {
-          return response.status(500).send(error);
-      }
-      response.send(result);
-  });
-});*/
+app.options('*', cors());
+
+app.get('/products', (request, response) => {
+  response.send({ 'ack': true });
+});
+
+app.listen(PORT, () => {
+  console.log(`📡 Running on port ${PORT}`);
+});
+
+app.get('/products/search', async (request, response) => {
+  try{
+    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    const db =  client.db(MONGODB_DB_NAME)
+    const collection = db.collection('products');
+    const { limit = 12, brand = 'All_brands', price = 1000 } = request.query;
+    const query = {
+      "brand": brand !== "All_brands" ? brand : { $exists: true }, // Filter by brand if specified, otherwise return all brands
+      "price": { $lte: parseFloat(price)} // Filter by price if specified, otherwise return all prices
+    };
+    const searchresult = await collection.find(query).sort({price : -1}).limit(parseInt(limit)).toArray();
+    response.send({result : searchresult});
+    client.close();
+  } catch(e){
+    response.send({error : "invalid search"});  
+  }
+});
+
+app.get('/products/:id', async(request, response) => {
+  try{
+    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+    const db =  client.db(MONGODB_DB_NAME)
+    const collection = db.collection('products');
+    const productId = request.params.id;
+    const searchresult = await collection.findOne({_id: ObjectId(productId)});
+    response.send({result : searchresult});
+    client.close();
+  } catch(e){
+    response.send({error : "invalid id"});  
+  }
+});
 
 
-/*app.get('/products/search', (request, response) => {
-  const { limit = 12, brand = 'All_brands', price = 10000 } = request.query;
-  const query = {
-    "brand": brand !== "All_brands" ? brand : { $exists: true }, // Filter by brand if specified, otherwise return all brands
-    "price": { $lte: parseFloat(price)} // Filter by price if specified, otherwise return all prices
-  };
-  collection.find(query).sort({price : -1}).limit(parseInt(limit)).toArray((err, result) => {
-    if (err) throw err;
-    response.send(result);
-  });
-});*/
-
-
-/*app.listen(PORT, () => {
-  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
-      if(error) {
-          throw error;
-      }
-      database = client.db(DATABASE_NAME);
-      collection = database.collection("products");
-      console.log("Connected to `" + DATABASE_NAME + "`!");
-  });
-});*/
-
-app.listen(PORT);
-
-console.log(`📡 Running on port ${PORT}`);
 
